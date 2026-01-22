5f0fee5cd13150376da93d7c8364ac1d17391334
VenkatReddy Kunam <venkat@gmail.com>
2026-01-22 17:02:47 +0530

    Day-01-Terraform

--- Day-01/main.tf
+++ Day-01/main.tf
@@ -1,34 +1,36 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.49"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 4.49"
+#     }
+#   }
+# }
+#
+# provider "azurerm" {
+#   features {}
+#   subscription_id = "af504235-2f6d-4469-aa25-251f498730fc"
+# }
+#
+# resource "azurerm_resource_group" "rg" {
+#   name     = "learning-resources"
+#   location = "UK West"
+# }
+#
+# resource "azurerm_storage_account" "storage_account" {
+# # depends_on = [ azurerm_resource_group.rg ]
+#   name                     = "azdevopsvenkat"
+#   resource_group_name      = azurerm_resource_group.rg.name  # implicit dependency
+#   location                 = azurerm_resource_group.rg.location # implicit dependency
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#
+#   tags = {
+#     environment = "Dev"
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "af504235-2f6d-4469-aa25-251f498730fc"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "learning-resources"
-  location = "UK West"
-}
-
-resource "azurerm_storage_account" "storage_account" {
-# depends_on = [ azurerm_resource_group.rg ]
-  name                     = "azdevopsvenkat"
-  resource_group_name      = azurerm_resource_group.rg.name  # implicit dependency
-  location                 = azurerm_resource_group.rg.location # implicit dependency
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "Dev"
-  }
-}
+##===========================
 ##if we need to configure Explicit dependency we pass dependency [] specifically on each resource .
 # depends_on = [ azurerm_resource_group.rg ]
 ##if you delete/comment storage resource it will validate the state file whether resource is there or not if not it will be destroyed
--- Day-01/statefile.tf
+++ Day-01/statefile.tf
@@ -1,6 +1,7 @@
 # Best Practice:
 # The storage account must be pre-created.
 # Terraform should only reference it, not create it in this main.tf file configuration.
+##by using shell script we will create storage account in another resource group
 
 #Terraform State File
 # Terraform uses the state file as the source of truth and reconciles differences between
@@ -8,3 +9,45 @@
 #Terraform state should always be stored in a remote backend to ensure consistency, security, and collaboration.
 #Terraform state files should never be manually modified; all state changes must be performed using
 # Terraform commands to avoid corruption.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.49"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "learning-tf-rg"
+    storage_account_name = "azdevops12574"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "af504235-2f6d-4469-aa25-251f498730fc"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "learning-resources"
+  location = "UK West"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  # depends_on = [ azurerm_resource_group.rg ]
+  name                     = "azdevopsvenkat"
+  resource_group_name      = azurerm_resource_group.rg.name  # implicit dependency
+  location                 = azurerm_resource_group.rg.location # implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Dev"
+  }
+}
