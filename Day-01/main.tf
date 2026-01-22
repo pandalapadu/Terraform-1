f02ffb4123a449d83f4135818bb3f5a952673b74
VenkatReddy Kunam <venkat@gmail.com>
2026-01-22 13:29:42 +0530

    Day-01-Terraform

--- Day-01/main.tf
+++ Day-01/main.tf
@@ -6,3 +6,21 @@ terraform {
     }
   }
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "learning-resources"
+  location = "UK West"
+}
+
+resource "azurerm_storage_account" "example" {
+
+  name                     = "azdevopsvenkat"
+  resource_group_name      = azurerm_resource_group.rg.name  # implicit dependency
+  location                 = azurerm_resource_group.rg.location # implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Dev"
+  }
+}
