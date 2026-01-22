d1e525917d6c30f22cab2f1050716a9864a7d275
VenkatReddy Kunam <venkat@gmail.com>
2026-01-22 22:01:42 +0530

    Terraform

--- 04-TF-Variable-TypeConstraints/04-07.main.tf
+++ 04-TF-Variable-TypeConstraints/04-07.main.tf
@@ -34,7 +34,7 @@ resource "azurerm_virtual_machine" "main" {
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = var.vm_config.size
+  vm_size               = var.vm_config.size[0]
 
   delete_os_disk_on_termination = var.is_delete
 
